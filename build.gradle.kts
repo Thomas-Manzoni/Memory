66cc28c64480cdb1e38ff91610aa53b34bb22042
MAT1LUD <thomas.manzoni@se.bosch.com>
2025-03-31 15:13:42 +0200

    Test commit with new json but maybe not synced

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Use AGP 8.5.2 to match the supported Gradle version
+        classpath("com.android.tools.build:gradle:8.5.2")
+    }
 }
